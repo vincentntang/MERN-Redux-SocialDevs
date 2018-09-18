f0b058768c1b21f96158ae100f2b49421b66c17e
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-09-18 19:54:24 -0400

    Connect MongoDB with Mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://vincent1:vincent1@ds261302.mlab.com:61302/kj_devconnectorz'
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
 
