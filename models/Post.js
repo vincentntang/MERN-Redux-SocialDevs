3f67e025af8f2fdfa63f8885679829b569a3748c
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-09-28 22:31:24 -0400

    25_createPostRoute_ButNoAvatar

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
-const PostScehma = new Schema({
+const PostSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: 'users'
