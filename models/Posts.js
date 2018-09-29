acd962eaf1ad28f5f2407cc92b9205eeefda08e4
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-09-28 22:06:48 -0400

    24_ Post Model

---
+++ models/Posts.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const PostScehma = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    require: true
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String
+  },
+  like: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comment: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        require: true
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
