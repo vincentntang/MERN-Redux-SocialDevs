528e70f7096b9508860fddf6ae8c76988f45ccbb
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-09-29 19:08:03 -0400

    Fixed posts data. Added prettier

--- validation/post.js
+++ validation/post.js
@@ -1,17 +1,17 @@
-const Validator = require('validator');
-const isEmpty = require('./is-empty');
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
 
-module.exports = function validateLoginInput(data) {
+module.exports = function validatePostInput(data) {
   let errors = {};
 
-  data.text = !isEmpty(data.text) ? data.text : '';
+  data.text = !isEmpty(data.text) ? data.text : "";
 
-  if (!Validator.isLength(data.text, { min: 10, max: 300})){
-    errors.text = 'Post must be between 10 and 300 characters';
+  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = "Post must be between 10 and 300 characters";
   }
 
   if (Validator.isEmpty(data.text)) {
-    errors.text = 'Text field is required';
+    errors.text = "Text field is required";
   }
 
   return {
