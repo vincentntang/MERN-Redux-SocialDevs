ee99ff88c2b6cc17715346a44926aa36fde9ba40
ACpython <10290348+Kagerjay@users.noreply.github.com>
2019-03-14 12:49:47 -0400

    62-ProfileSkillsNotRendering

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
+import ProfileItem from "./ProfileItem";
 import { getProfiles } from "../../actions/profileActions";
 
+// For the developers page
 class Profiles extends Component {
   componentDidMount() {
     this.props.getProfiles();
@@ -16,7 +18,9 @@ class Profiles extends Component {
       profileItems = <Spinner />;
     } else {
       if (profiles.length > 0) {
-        <h1>PROFILES HERE</h1>;
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ));
       } else {
         profileItems = <h4>No profiles found...</h4>;
       }
