30ae75bbc71eb99a54a9df23da916640cb372619
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-03-17 23:53:39 -0400

    add Comment Form

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
 import Spinner from "../common/Spinner";
 import { getPost } from "../../actions/postActions";
 
@@ -20,6 +21,7 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
