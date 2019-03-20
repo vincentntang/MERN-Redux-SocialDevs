67ad18a0d7ec01abe5daf78e93f26d86588f8086
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-03-19 23:54:46 -0400

    Deploy 2

--- server.js
+++ server.js
@@ -14,17 +14,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Hackhands Production CORS allow for port 3000 talking to port 5000
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-// DB config
+// DB Config
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
@@ -49,7 +39,6 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  // Load react index.html file
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
